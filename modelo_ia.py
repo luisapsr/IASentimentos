b37f7cba2b8c229c1d4953c0714ae786272202c8
Luisa Romeiro <luisape.romeiro@gmail.com>
2025-11-10 11:36:52 -0300

    Atualizacao 3

--- modelo_ia.py
+++ modelo_ia.py
@@ -21,6 +21,7 @@ def limpar_texto(texto):
 
 # --- Carregar dataset ---
 print("📂 Carregando dataset...")
+DATASET_PATH = "dataset_sentimentos.csv"
 df = pd.read_csv(DATASET_PATH, sep=';', on_bad_lines='skip', encoding='utf-8')
 
 # Verifica se as colunas existem
